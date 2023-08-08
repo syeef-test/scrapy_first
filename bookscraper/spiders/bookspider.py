9d305a57091edcbb72220b77499a6659f33f6382
kazi <kazi@localhost.localdomain>
2023-08-08 20:28:45 +0530

    books data added to mysql database part 7 done

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -7,6 +7,13 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ['books.toscrape.com']
     start_urls = ['https://books.toscrape.com/']
 
+
+    custom_settings = {
+        'FEEDS':{
+            'booksdata.json':{'format':'json','overwrite':True}
+        }  
+    }
+
     def parse(self, response):
         books = response.css('article.product_pod')
         for book in books:
